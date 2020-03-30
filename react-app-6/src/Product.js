212d4b911abdc88793ccfe355a1d6ca710a72a54
bug-dor <tolyaumer@rambler.ru>
2020-03-30 22:44:06 +0300

    used toLocaleString in Product

--- react-app-6/src/Product.js
+++ react-app-6/src/Product.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import './product.css'
 
 function Product(props) {
     return (
-        <div>
-            <h1>{props.name}</h1>
-            <p>{props.price}</p>
-            <p>{props.description}</p>
+        <div className="product">
+            <div className="product__name">{props.product.name}</div>
+            <div className="product__price">price: {props.product.price.toLocaleString("en-US",{style:"currency",currency:"USD"})}</div>
+            <div className="product__desc">{props.product.description}</div>
         </div>
     )
 }
